03eb95226b9dad2eca559faed9116613bba1152e
Oscar Manuel <ozkar@MacBook-Pro-de-Oscar.local>
2026-04-30 12:18:29 +0200

    Arreglo de  el error de IP Fija y comunicacion con el servidor al registar

--- frontend/js/api-dashboard.js
+++ frontend/js/api-dashboard.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000/api/repuestos.php';
+const API_URL = `${window.location.protocol}//${window.location.hostname}:8000/api/repuestos.php`;
 
 document.addEventListener('DOMContentLoaded', () => {
 
